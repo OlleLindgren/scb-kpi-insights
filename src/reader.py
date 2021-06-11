4ae624632b172a918b6fc2f428af63910438244c
Olle Lindgren <lindgrenolle@live.se>
2021-06-11 17:05:36 +0200

    successfully parsing data

--- src/reader.py
+++ src/reader.py
@@ -4,18 +4,21 @@ import os
 
 from settings import DATA_DIR
 
+def _read_csv(filename):
+    result = pd.read_csv(
+        filename, 
+        encoding='latin1', 
+        index_col=0
+    ).replace('..', np.nan)
+    for col in result.columns:
+        result[col] = result[col].astype(float)
+    return result
 
-with open(os.path.join(DATA_DIR, 'years-items-values.csv'), 'rb') as f:
-    for i, line in enumerate(f.readlines()):
-        if i < 3:
-            continue
-        if i == 3:
-            print(line)
-        if i > 3:
-            break
+values = _read_csv(os.path.join(DATA_DIR, 'values.csv'))
+weights = _read_csv(os.path.join(DATA_DIR, 'weights.csv'))
 
-values = pd.read_csv(os.path.join(DATA_DIR, 'years-items-values.csv'), delimiter=',')
-weights = pd.read_csv(os.path.join(DATA_DIR, 'years-items-weights.csv'), delimiter=',')
+diffs = (values - values.shift(1, axis=1)).drop(columns=values.columns[0])
+log_diffs = (values.apply(np.log) - values.shift(1, axis=1).apply(np.log)).drop(columns=values.columns[0])
 
-print(values)
-print(weights)
+weighted_diffs = diffs.multiply(weights)[weights.columns.intersection(diffs.columns)]
+weighted_log_diffs = log_diffs.multiply(weights)[weights.columns.intersection(diffs.columns)]
